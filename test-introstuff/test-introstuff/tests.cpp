4b7e29b2051eb18c94f6232659bcfef35007b50f
smeno004 <siddharth.krishnaleela@gmail.com>
2016-11-04 12:19:43 -0700

    tests.cpp updated
    tests started

--- test-introstuff/test-introstuff/tests.cpp
+++ test-introstuff/test-introstuff/tests.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include "opencv2\core\core.hpp"
 
 using namespace std;
 
 int main()
 {
 	cout << "MoDe 3d Drawing/Manipulation Program..." << endl;
+
+	int input;
+
+	cin >> input;
 
 	return 0;
 }
