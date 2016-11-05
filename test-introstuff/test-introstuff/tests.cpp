eb611783bc4d96225e653b3ec8d78616cfa8a8e0
smeno004 <siddharth.krishnaleela@gmail.com>
2016-11-04 23:55:29 -0700

    Learning to use OpenCV

--- test-introstuff/test-introstuff/tests.cpp
+++ test-introstuff/test-introstuff/tests.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 #include "opencv2\core\core.hpp"
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/photo/photo.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/core/core_c.h"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
 
 using namespace std;
+using namespace cv;
 
 int main()
 {
-	cout << "MoDe 3d Drawing/Manipulation Program..." << endl;
+	Mat img(1000, 1000, CV_8UC3, Scalar(100, 25, 50));
 
-	int input;
+	if (img.empty())
+	{
+		cout << "Error : Image could not be loaded!" << endl;
+		return -1;
+	}
 
-	cin >> input;
+	namedWindow("Image Read", CV_WINDOW_NORMAL);
+
+	imshow("Image Read", img);
+
+	waitKey(0);
+
+	destroyWindow("Image Read");
 
 	return 0;
 }
